ce9e2c8ad2c9023cc82a8f730144653061e54c1e
BenPfister03 <benpfister03@gmail.com>
2025-04-16 22:07:10 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// config.js
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "localbites.firebaseapp.com",
+  projectId: "localbites",
+  storageBucket: "localbites.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
